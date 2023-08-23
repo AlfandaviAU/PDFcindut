69bdde217e947320e640df1d48df9a9a8f96a984
Alfandavi Aryo Utomo <13519211@std.stei.itb.ac.id>
2023-08-24 03:21:15 +0700

    updates

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { Button,Alert, AlertIcon,HStack, AlertTitle, AlertDescription, ButtonGroup,Input, Table, TableCaption, Thead, Tbody, Tr, Th, Td, Tfoot, Card, CardBody, Text, CardFooter, Container, Divider, Heading, Stack, Center, Flex, TableContainer } from "@chakra-ui/react";
+import { Button,HStack, Input, Table, Thead, Tbody, Tr, Th, Td, Card, CardBody, Text, CardFooter, Container, Heading, Stack, Center, TableContainer } from "@chakra-ui/react";
 
 import axios from "axios";
 
